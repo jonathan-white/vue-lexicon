60ce2bfd4c3e5ad5ba47197c90e97719705a642d
jonathan-white <33507081+jonathan-white@users.noreply.github.com>
2021-07-11 18:18:55 -0400

    updated endpoint parameters

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const CORS = "https://cors-anywhere.herokuapp.com/";
-const OXFORD_ENDPOINT = "https://od-api.oxforddictionaries.com:443/api/v2/entries/en-gb/";
+const OXFORD_ENDPOINT = "https://od-api.oxforddictionaries.com/api/v2/entries/en-us/";
 // const URBAN_ENDPOINT = "https://mashape-community-urban-dictionary.p.mashape.com/define?term=";
 
 export default {
   oxfordSingleWord: function(wordId) {
-    return axios.get(`${CORS}${OXFORD_ENDPOINT}${wordId}`,{
+    return axios.get(`${CORS}${OXFORD_ENDPOINT}${wordId}?strictMatch=false`,{
       headers: {
         "Accept": "application/json",
         "app_id": process.env.VUE_APP_OXFORD_APP_ID,
