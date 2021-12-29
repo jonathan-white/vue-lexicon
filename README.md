45d3af7c85e35ae083cd18a9caf8932e3db67cc8
Jonathan White <33507081+jonathan-white@users.noreply.github.com>
2021-12-28 20:56:47 -0500

    readme and API updates

--- README.md
+++ README.md
@@ -3,3 +3,42 @@
 ## Purpose
 
 My Lexicon is an educational app that lets users keep track of their personal lexicon. As users learn and add more words they can quickly view their growing lexicon through this tool.
+
+--- 
+
+## Getting Started:
+
+1. Install the dependencies
+
+`
+$ npm install
+`
+
+2. Obtain an API key from [Oxford Dictionaries](https://developer.oxforddictionaries.com/)
+
+3. Create a [Firebase Web app](https://console.firebase.google.com/).
+
+4. Create a `.env` file in the root folder of your project and add the following:
+
+```
+VUE_APP_FIREBASE_API_KEY=[YOUR_FIREBASE_API_KEY]
+
+VUE_APP_FIREBASE_DB_URL=[FIREBASE_PROJECT_URL]
+
+VUE_APP_FIREBASE_DOMAIN=[FIREBASE_PROJECT_DOMAIN]
+
+VUE_APP_FIREBASE_PROJECT_ID=[FIREBASE_PROJECT_ID]
+
+VUE_APP_FIREBASE_SENDER_ID=[FIREBASE_SENDER_ID]
+
+VUE_APP_FIREBASE_STORAGE=[FIREBASE_STORAGE]
+
+VUE_APP_OXFORD_APP_ID=[YOUR_OXFORD_APP_ID]
+
+VUE_APP_OXFORD_APP_KEY=[YOUR_OXFORD_API_KEY]
+```
+
+5. Run the app
+`
+$ npm run serve
+`
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const CORS = "https://cors-anywhere.herokuapp.com/";
-const OXFORD_ENDPOINT = "https://od-api.oxforddictionaries.com/api/v2/entries/en-us/";
+// const CORS = "https://cors-anywhere.herokuapp.com/";
+// const OXFORD_ENDPOINT = "https://od-api.oxforddictionaries.com/api/v2/entries/en-us/";
 // const URBAN_ENDPOINT = "https://mashape-community-urban-dictionary.p.mashape.com/define?term=";
 
 export default {
   oxfordSingleWord: function(wordId) {
-    return axios.get(`${CORS}${OXFORD_ENDPOINT}${wordId}?strictMatch=false`,{
+    return axios.get(`https://od-api.oxforddictionaries.com/api/v2/entries/en-us/${wordId}?strictMatch=false`,{
       headers: {
         "Accept": "application/json",
         "app_id": process.env.VUE_APP_OXFORD_APP_ID,
@@ -14,7 +14,7 @@ export default {
     });
   },
   oxfordSynonyms: function(wordId) {
-    return axios.get(`${CORS}${OXFORD_ENDPOINT}${wordId}/synonyms`, {
+    return axios.get(`https://od-api.oxforddictionaries.com/api/v2/entries/en-us/${wordId}/synonyms`, {
       headers: {
         "Accept": "application/json",
         "app_id": process.env.VUE_APP_OXFORD_APP_ID,
